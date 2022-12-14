90c7d593813d9192a920b925c4922de6c39a83b5
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2022-12-15 04:24:13 +0900

    edit talker.py

--- mypkg/mypkg/talker.py
+++ mypkg/mypkg/talker.py
@@ -2,17 +2,20 @@ import rclpy                     #ROS2のクライアントのためのライブ
 from rclpy.node import Node      #ノードを実装するためのNodeクラス（クラスは第10回で）
 from std_msgs.msg import Int16   #通信の型（16ビットの符号付き整数）
 
+class Talker():
+    def __init__(self):
+        self.pub = node.create_publisher(Int16, "countup", 10)
+        self.n = 0
+
 rclpy.init()
 node = Node("talker")            #ノード作成（nodeという「オブジェクト」を作成）
-pub = node.create_publisher(Int16, "countup", 10)   #パブリッシャのオブジェクト作成
-n = 0 #カウント用変数
+talker = Talker()
 
 def cb():          #17行目で定期実行されるコールバック関数
-    global n       #関数を抜けてもnがリセットされないようにしている
     msg = Int16()  #メッセージの「オブジェクト」
-    msg.data = n   #msgオブジェクトの持つdataにnを結び付け
-    pub.publish(msg)        #pubの持つpublishでメッセージ送信
-    n += 1
+    msg.data = talker.n   #msgオブジェクトの持つdataにnを結び付け
+    talker.pub.publish(msg)        #pubの持つpublishでメッセージ送信
+    talker.n += 1
 
 node.create_timer(0.5, cb)  #タイマー設定
 rclpy.spin(node)            #実行（無限ループ）
