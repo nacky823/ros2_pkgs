df975c05db55f4b025a4295939bbfef0c43dd52e
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2022-12-15 04:59:00 +0900

    add class talker.py

--- mypkg/mypkg/talker.py
+++ mypkg/mypkg/talker.py
@@ -3,19 +3,22 @@ from rclpy.node import Node      #ノードを実装するためのNodeクラス
 from std_msgs.msg import Int16   #通信の型（16ビットの符号付き整数）
 
 class Talker():
-    def __init__(self):
-        self.pub = node.create_publisher(Int16, "countup", 10)
+    def __init__(self, nh):
+        self.pub = nh.create_publisher(Int16, "countup", 10)
         self.n = 0
+        nh.create_timer(0.5, self.cb)  #タイマー設定
 
-rclpy.init()
-node = Node("talker")            #ノード作成（nodeという「オブジェクト」を作成）
-talker = Talker()
+    def cb(self):          #17行目で定期実行されるコールバック関数
+        msg = Int16()  #メッセージの「オブジェクト」
+        msg.data = self.n   #msgオブジェクトの持つdataにnを結び付け
+        self.pub.publish(msg)        #pubの持つpublishでメッセージ送信
+        self.n += 1
 
-def cb():          #17行目で定期実行されるコールバック関数
-    msg = Int16()  #メッセージの「オブジェクト」
-    msg.data = talker.n   #msgオブジェクトの持つdataにnを結び付け
-    talker.pub.publish(msg)        #pubの持つpublishでメッセージ送信
-    talker.n += 1
+def main():
+    rclpy.init()
+    node = Node("talker")            #ノード作成（nodeという「オブジェクト」を作成）
+    talker = Talker(node)
+    rclpy.spin(node)            #実行（無限ループ）
 
-node.create_timer(0.5, cb)  #タイマー設定
-rclpy.spin(node)            #実行（無限ループ）
+if __name__ == '__main__':
+    main()
